d5f7fca926b20b17d81794accbce3e4c6a5d5d68
Serda Simus <iamserda@gmail.com>
2021-06-02 10:41:58 -0400

    updated assignments

--- serdaSimus/assignments/slidingwindow/lc53/lc53.js
+++ serdaSimus/assignments/slidingwindow/lc53/lc53.js
@@ -1,6 +1,6 @@
 
-
-
+// Problem Statement #
+// Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
 
 function max_subarray_size_k(k, arr) {
   // TODO: Write your code here
