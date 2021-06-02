dff62ace22531596550c7353e1dea10405f5e725
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-02 19:44:30 -0400

    debug solution

--- joshuaCarpentier/assignments/slidingwindow/lc209/lc209.js
+++ joshuaCarpentier/assignments/slidingwindow/lc209/lc209.js
@@ -1,28 +1,28 @@
 
 
 
-// function smallest_subarray_with_given_sum(s, arr) {
-//   // TODO: Write code here
-// }
+function smallest_subarray_with_given_sum(s, arr) {
+// debugged solution
+ }
 
-// console.log(
-//   `Smallest subarray length: ${smallest_subarray_with_given_sum(
-//     7,
-//     [2, 1, 5, 2, 3, 2]
-//   )}`
-// );
-// console.log(
-//   `Smallest subarray length: ${smallest_subarray_with_given_sum(
-//     7,
-//     [2, 1, 5, 2, 8]
-//   )}`
-// );
-// console.log(
-//   `Smallest subarray length: ${smallest_subarray_with_given_sum(
-//     8,
-//     [3, 4, 1, 1, 6]
-//   )}`
-// );
+console.log(
+  `Smallest subarray length: ${smallest_subarray_with_given_sum(
+    7,
+    [2, 1, 5, 2, 3, 2]
+  )}`
+);
+console.log(
+  `Smallest subarray length: ${smallest_subarray_with_given_sum(
+    7,
+    [2, 1, 5, 2, 8]
+  )}`
+);
+console.log(
+  `Smallest subarray length: ${smallest_subarray_with_given_sum(
+    8,
+    [3, 4, 1, 1, 6]
+  )}`
+);
 
 // Solution
 // -----
