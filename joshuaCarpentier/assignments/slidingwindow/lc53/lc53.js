cabb311f251fcb71cc25c8421bd8f26defbaa948
jacgit18 <joshuacarpentier21@gmail.com>
2021-06-02 18:53:56 -0400

    Update lc53.js

--- joshuaCarpentier/assignments/slidingwindow/lc53/lc53.js
+++ joshuaCarpentier/assignments/slidingwindow/lc53/lc53.js
@@ -6,29 +6,60 @@ k is basically window size across array index
 
 reutn zero or  null if the array length is zero or k is 0
 
-
-
 */
 
 function max_subarray_size_k(k, arr) {
-  let maxSum = 0,
-  winStart = 0,
-  windSum = 0;
 
-  //
+  // OG Attempt
+  // let maxSum = 0,
+  // winStart = 0,
+  // windSum = 0;
 
-  for (let winEnd = 0; winEnd < arr.length; winEnd++){
-      if(arr[winEnd] == winEnd + 1){
-        windSum += arr[winEnd];
-        console.log(windSum)
+  
 
-      }
+  // for (let winEnd = 0; winEnd < arr.length; winEnd++){
+  //     if(arr[winEnd] == winEnd + 1){
+  //       windSum += arr[winEnd];
+  //       console.log(windSum)
 
+  //     }
+
+  // }
+
+
+  //Brute Force Solution
+// let maxSum = 0,
+//     winSum = 0;
+
+//     for (let i = 0; i < arr.length + 1 - k; i++){
+//       winSum = 0;
+//       for (let j = i; j < i + k; j++ ){
+//         winSum += arr[j];
+//       }
+//       maxSum = Math.max(maxSum, winSum);
+//     }
+
+//     return console.log(maxSum);
+
+
+  //Better Approach Solution
+let maxSum = 0,
+winSum = 0,
+winStart = 0;
+
+for (let winEnd = 0; winEnd < arr.length; winEnd++ ){
+  winSum += arr[winEnd];
+  if (winEnd >= k - 1){
+    maxSum = Math.max(maxSum, winSum);
+    winSum -= arr[winStart];
+    winStart += 1;
   }
-
-
+}
+return console.log(maxSum);
 
 }
+
+
 
 console.log(
   `Maximum sum of a subarray of size K: ${max_subarray_size_k(
@@ -57,36 +88,3 @@ console.log(
   )}`
 );
 
-// bruteforce/naive approach
-// -----
-// let maxSum = 0,
-// windowSum = 0;
-
-// for (i = 0; i < arr.length - k + 1; i++) {
-//   windowSum = 0;
-//   for (j = i; j < i + k; j++) {
-//     windowSum += arr[j];
-//   }
-//   maxSum = Math.max(maxSum, windowSum);
-// }
-// return maxSum;
-
-// better approach
-// -----
-// let maxSum = 0,
-// windowSum = 0,
-// windowStart = 0;
-
-// for (window_end = 0; window_end < arr.length; window_end++) {
-//   // add the next element
-//   windowSum += arr[window_end];
-//   // slide the window, we don't need to slide if we've not hit the required window size of 'k'
-//   if (window_end >= k - 1) {
-//     maxSum = Math.max(maxSum, windowSum);
-//     // subtract the element going out
-//     windowSum -= arr[windowStart];
-//     // slide the window ahead
-//     windowStart += 1;
-//   }
-// }
-// return maxSum;
