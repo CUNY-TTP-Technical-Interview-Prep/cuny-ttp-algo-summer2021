2a0b6468bf839466c66b9acbdcec6b0d902de4cd
Serda Simus <iamserda@gmail.com>
2021-06-02 12:24:49 -0400

    Serda's solution to LC53 in JS

--- serdaSimus/assignments/slidingwindow/lc53/lc53.js
+++ serdaSimus/assignments/slidingwindow/lc53/lc53.js
@@ -1,11 +1,41 @@
-
 // Problem Statement #
 // Given an array of positive numbers and a positive number ‘k,’ find the maximum sum of any contiguous subarray of size ‘k’.
 
+/*
+  [] ... [1] ... [1,2,3,4,5]
+  I need accumulator: sum
+  I need var: largestValue
+  I need a counter: 
+
+  for loop: 
+    add each item to sum
+    check if counter >= k:
+      compare sum and largest: if sum is bigger:
+        i would save sum to larges, else I would continue
+        sum = sum - arr[counter - k]
+    counter++
+
+  return largestValue
+      
+*/
+// [1,2,3,4,6] k=3
+
 function max_subarray_size_k(k, arr) {
-  // TODO: Write your code here
+  if (!arr.length || k >= arr.length || k < 0) return null;
 
+  let sum = 0;
+  let largest = 0;
+  let counter = 1;
 
+  for (let num of arr) {
+    sum += num;
+    if (counter >= k) {
+      largest = sum > largest ? sum : largest;
+      sum -= arr[counter - k];
+    }
+    counter++;
+  }
+  return largest;
 }
 
 console.log(
@@ -19,6 +49,13 @@ console.log(
     2,
     [2, 3, 4, 1, 5]
   )}`
+);
+console.log(
+  `Maximum sum of a subarray of size K: ${max_subarray_size_k(2, [])}`
+);
+
+console.log(
+  `Maximum sum of a subarray of size K: ${max_subarray_size_k(2, [2])}`
 );
 
 // bruteforce/naive approach
