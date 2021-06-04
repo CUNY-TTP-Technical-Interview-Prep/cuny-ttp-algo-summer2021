e436e1f8ae6ced3e30e733af10c6d5dd2f57c165
simplisticmartin <simplisticmartin@gmail.com>
2021-06-03 20:37:08 -0400

    lc15v1

--- martinLi/assignments/twopointers/lc15/lc15v1.js
+++ martinLi/assignments/twopointers/lc15/lc15v1.js
@@ -1,98 +1,31 @@
 // Problem Statement #
 // Given an array of unsorted numbers, 
-// find all unique triplets in it that add up to zero.
-/*
-
-if length less than 3 return empty
-
-arr = [...]
-sort the array
-
-3 unique numebrs which add up to 0 can't be the same combinations
-
-/*arr is sorted*
-left =0 right =leng-1
-while(left < right){
-target_sum = -(arr[left] + arr[right])//-1 => 1
-for num in arr{
-  if target_sum == num
-    triplets.push(arr[left], arr[right], num)
-}
-left = left + 1
-right = right - 1
- 
-
-  }
-[-3,-2, -1, 0, 1, 1, 2]
-  l
-                     r
-ts = -1
-
-
-console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2]));
-
-while(arr[i] <= target_sum){
-  if(target_sum == arr[i])
-    triplets.push(arr[left], arr[right], arr[i])
-}
-*/
+//find all unique triplets in it that add up to zero.
 
 function search_triplets(arr) {
-  //add up to 0
-  let target_sum = 0;
-  let left = 0;
-  let triplets = []
-  let right = arr.length -1;
+  let triplets = [];
+  let left = 0, right = arr.length - 1;
   while(left < right){
-    const current_sum = arr[left] + arr[right];
-    //target_sum = arr[left]+ arr[right]
-    if(current_sum === target_sum ){
-      triplets.push([-target_sum, arr[left], arr[right]]);
-      left += 1;
-      right -= 1;
-      while(left < right && arr[left] === arr[left-1])
-      {
-        left +=1;
+    let target_sum = -(arr[left] + arr[right])
+    for(num in arr) {
+      if(target_sum == arr[num]){
+      triplets.push([arr[left], arr[right], arr[num]])
       }
-      while(left < right && arr[right] === arr[right + 1])
-      {
-        right -= 1;
-      } 
     }
-    else if (target_sum > current_sum)
-    {
-      left += 1;
-    }
-    else {
-      right -=1;
-    }
-  } 
-  ///let triplets = [], left = 0, right = arr.length-1;
- // arr.sort()
-  console.log(arr)
-
+    left = left + 1;
+    right = right -1;
+  }
 
   // TODO: Write your code here
   return triplets;
 }
 
 console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2]));
-/*
-l
-                     r
--3, 0, 1, 2, -1, 1, -2
-i = 0
-
-
-*/
-
-
-console.log(search_triplets([-5, 2, -1, -2, 3]));
+//console.log(search_triplets([-5, 2, -1, -2, 3]));
 
 // Solution
 // -----
-// arr.sort((a, b) => a - b); lowest to highest
-// arr.sort()
+// arr.sort((a, b) => a - b);
 // const triplets = [];
 // for (i = 0; i < arr.length; i++) {
 //   if (i > 0 && arr[i] === arr[i - 1]) { // skip same element to avoid duplicate triplets
