42187f43598144562465ed658f8946ca52160bf6
Alex Tan <AlexTAN331@outlook.com>
2021-06-06 15:25:26 -0400

    feat: finish lc #15 and mark it as completed

--- taojieTan/assignments/twopointers/lc15/lc15.py
+++ taojieTan/assignments/twopointers/lc15/lc15.py
@@ -1,24 +1,70 @@
 # Problem Statement #
 # Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
+
 def search_triplets(arr):
-  triplets = []
-  # TODO: Write your code here
-  return triplets
+    # TODO: Write your code here
+    # input: an unsorted integer array (positive or/and negative)
+    # output: an array with unique triplets eqauls to zero
+
+    # edge cases: 1. length of array is smaller than 3 => return []
+    #            2. only zero in the array => return [0, 0, 0]
+
+    # initialize an array to store all unique triplets where the sum is equal to zero
+    triplets = []
+
+    # first sort the array in order to utilize the two pointers pattern
+    arr.sort()
+
+    # loop through the array from zero to third to last
+    # first check if there exists cases where current value is equal to previous one, if so, skip it to prevent duplicate triplet;
+    # if not, we initialize two pointers, left and right, to point to leftmost and rightmost value in the array
+    # sum the three values with indices of i, leftmost, and rightmost, and check if it eqauls to zero.
+    # if not, we have two cases:
+    # 1.it is greater than zero, it means we need to shift right pointer toward left for smaller sum,
+    # 2.it is smaller than zero, it means we need to shift left pointer toward right for larger sum.
+    # if the sum is equal zero, we first append these value to result array,
+    # and then we check if the next values are same as the ones pointed by left and right pointers
+    # we skip the duplicate value to prevent storing duplicate triplet into result array.
+    # and we also need to shrink both left and right pointers toward the middle.
+    # when finish the for loop, we will get the final result
+
+    for i in range(len(arr) - 2):
+        if arr[i] == arr[i - 1] and i > 0:
+            continue
+
+        left = i + 1
+        right = len(arr) - 1
+
+        while left < right:
+            sum = arr[i] + arr[left] + arr[right]
+            if sum > 0:
+                right -= 1
+            elif sum < 0:
+                left += 1
+            else:
+                triplets.append((arr[i], arr[left], arr[right]))
+                while left < right and arr[left] == arr[left + 1]:
+                    left += 1
+                while left < right and arr[right] == arr[right - 1]:
+                    right -= 1
+
+                left += 1
+                right -= 1
+
+    return triplets
 
 
 def main():
-  print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
-  print(search_triplets([-5, 2, -1, -2, 3]))
+    print(search_triplets([-13, 0, 12, 2, -5, 1, -2, 3, -1, 5]))
+    print(search_triplets([-3, 0, 1, 2, -1, 1, -2]))
+    print(search_triplets([-5, 2, -1, -2, 3]))
+    print(search_triplets([0, 0, 0, 0, 0]))
+    print(search_triplets([0, 0]))
+    print(search_triplets([]))
 
 
 main()
-
-
-
-
-
-
 
 
 # Solution
