8a38cf47748566ff840a763c6de9d17e6b7bc479
ryan4summer <ryan4summer0527@gmail.com>
2021-06-07 09:15:30 -0400

    feat: update assignment files to name folder

--- tiantianXia/assignments/slidingwindow/lc904/lc904.js
+++ tiantianXia/assignments/slidingwindow/lc904/lc904.js
@@ -6,6 +6,22 @@
 
 // Write a function to return the maximum number of fruits in both baskets.
 
+/**
+ * input:
+ * @param {*} fruits: an array of the characters
+ * 
+ * output: maxNum the maximum number of fruits in both baskets
+ * 
+ * variables: windowStart, windowEnd: to operate a sliding window
+ * maxNum store the maximum number of the fruites
+ * frequency: store the frequency of each fruites
+ * 
+ * run a for loop to implement all the element in the array
+ * count the frequency of each fruit 
+ * 
+ * 
+ */
+
 function fruits_into_baskets(fruits) {
   // TODO: Write code here
 }
