788b22bc4676b54f84a30bf011a7cec4982630d1
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-07 14:49:54 -0400

    edit

--- joshuaCarpentier/assignments/twopointers/lc1/lc1.js
+++ joshuaCarpentier/assignments/twopointers/lc1/lc1.js
@@ -4,7 +4,9 @@
 // Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
 
 function pair_with_target_sum(arr, targetSum) {
-  // TODO:write code here
+
+
+
 }
 
 console.log(pair_with_target_sum([1, 2, 3, 4, 6], 6));
