c3dbf0520ef58c1964c71182d24aa7d6452bf6d4
ryan4summer <ryan4summer0527@gmail.com>
2021-06-07 15:26:38 -0400

    feat: complete lc15/lc15.js question

--- tiantianXia/assignments/twopointers/lc15/lc15.js
+++ tiantianXia/assignments/twopointers/lc15/lc15.js
@@ -1,14 +1,66 @@
 // Problem Statement #
 // Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
+/**
+ * input: 
+ * @param {*} arr: unsorted numbers doesn't positive number
+ * output:
+ * @returns triplets: all unique number that add up to zero.
+ * 
+ * varible left, right pointer to implement two pointer approach
+ * sort the arr 
+ * for loop to go through all the elements
+ * if the element is same as the last elements skip this element
+ * 
+ * while loop go through all the elements for the array left greater than the right
+ * left should i + 1, right legnth - 1.
+ * 
+ * compare if the arr[i] = arr[left] + arr [right]
+ *  triplets push([arr[i], arr[left], arr[right]])
+ * if (arr[i] + left + right < 0)
+ *  left ++
+ * else if (arr[i] + left + right > 0)
+ *  right --
+ * 
+ * 
+ * return triplets
+ * 
+ * edge case: empty arr => triplet = []
+ */
 function search_triplets(arr) {
-  triplets = [];
+  if(!arr.length) return [];
+  let triplets = [];
   // TODO: Write your code here
+
+  arr.sort((a,b) => a - b);
+
+  for(let i = 0; i < arr.length; i ++){
+    if(arr[i] === arr[i + 1])
+      continue;
+    let left = i + 1;
+    let right = arr.length - 1;
+    while (left < right){
+      // console.log(" firt: ", arr[i], "left: ", arr[left], "right: ", arr[right]);
+
+      if (arr[i] + arr[left] + arr[right] === 0){
+        triplets.push([arr[i], arr[left], arr[right]]);
+        left ++;
+        right --;
+      } else if (arr[i] + arr[left] + arr[right] < 0){
+        left ++
+      } else {
+        right --;
+      }
+    }
+  }
+
   return triplets;
 }
 
-console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2]));
+console.log(search_triplets([-3, 0, 1, 2, -1, 1, -2])); // [-3, -2, -1, 0, 1 ,1 ,2]
 console.log(search_triplets([-5, 2, -1, -2, 3]));
+console.log(search_triplets([]));
+console.log(search_triplets([-1, 2, 4, 8]));
 
 // Solution
 // -----
