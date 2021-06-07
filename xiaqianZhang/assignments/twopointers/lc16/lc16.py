1d4d0b8924b69506553d8e4fd2c1167e10c6c420
xiaqian1999 <xiaqian1999@gmail.com>
2021-06-07 09:38:59 -0400

    two pointer 16

--- xiaqianZhang/assignments/twopointers/lc16/lc16.py
+++ xiaqianZhang/assignments/twopointers/lc16/lc16.py
@@ -43,7 +43,8 @@ def triplet_sum_close_to_target(arr, target_sum):
       if currentDiff == 0: #This means that targetSum == resultSum of a+b+c
         return target_sum
 
-      if abs(currentDiff) < abs(smallestDiff):#Then te triplet sum is closer to the previous sum, update the difference
+      if abs(currentDiff) < abs(smallestDiff) or (abs(currentDiff) == abs(smallestDiff) and currentDiff < smallestDiff):
+        #Then te triplet sum is closer to the previous sum, update the difference
         smallestDiff = currentDiff
 
       if currentDiff > 0: #we want bigger numbers that could increase the sum
@@ -77,26 +78,26 @@ main()
 
 # Solution
 # -----
-  arr.sort()
-  smallest_difference = math.inf
-  for i in range(len(arr)-2):
-    left = i + 1
-    right = len(arr) - 1
-    while (left < right):
-      target_diff = target_sum - arr[i] - arr[left] - arr[right]
-      if target_diff == 0:  # we've found a triplet with an exact sum
-        return target_sum - target_diff  # return sum of all the numbers
+  # arr.sort()
+  # smallest_difference = math.inf
+  # for i in range(len(arr)-2):
+  #   left = i + 1
+  #   right = len(arr) - 1
+  #   while (left < right):
+  #     target_diff = target_sum - arr[i] - arr[left] - arr[right]
+  #     if target_diff == 0:  # we've found a triplet with an exact sum
+  #       return target_sum - target_diff  # return sum of all the numbers
 
-      # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-      if abs(target_diff) < abs(smallest_difference) or (abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
-        smallest_difference = target_diff  # save the closest and the biggest difference
+  #     # the second part of the following 'if' is to handle the smallest sum when we have more than one solution
+  #     if abs(target_diff) < abs(smallest_difference) or (abs(target_diff) == abs(smallest_difference) and target_diff > smallest_difference):
+  #       smallest_difference = target_diff  # save the closest and the biggest difference
 
-      if target_diff > 0:
-        left += 1  # we need a triplet with a bigger sum
-      else:
-        right -= 1  # we need a triplet with a smaller sum
+  #     if target_diff > 0:
+  #       left += 1  # we need a triplet with a bigger sum
+  #     else:
+  #       right -= 1  # we need a triplet with a smaller sum
 
-  return target_sum - smallest_difference
+  # return target_sum - smallest_difference
 
 # -----
 
