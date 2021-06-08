d65379e829f591d01e12e235a3c296a8e33c9613
kwu25 <kelvin.wu125@gmail.com>
2021-06-08 10:52:43 -0400

    Completed lc876

--- kelvinWu/fastSlow/lc876/lc876.js
+++ kelvinWu/fastSlow/lc876/lc876.js
@@ -5,33 +5,37 @@
 // If the total number of nodes in the LinkedList is even, return the second middle node.
 
 class Node {
-  constructor(value, next=null){
+  constructor(value, next = null) {
     this.value = value;
     this.next = next;
   }
 }
+//keep two pointers
+//one pointer moves twice as fast as other
+//return slow node after fast or fast.next reaches null
+const find_middle_of_linked_list = function (head) {
+  let slow = head,
+    fast = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
 
-const find_middle_of_linked_list = function(head) {
-  // TODO: Write your code here
-  return head;
-}
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
 
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`);
 
-head = new Node(1)
-head.next = new Node(2)
-head.next.next = new Node(3)
-head.next.next.next = new Node(4)
-head.next.next.next.next = new Node(5)
+head.next.next.next.next.next = new Node(6);
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`);
 
-console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
-
-head.next.next.next.next.next = new Node(6)
-console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
-
-head.next.next.next.next.next.next = new Node(7)
-console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`)
-
-
+head.next.next.next.next.next.next = new Node(7);
+console.log(`Middle Node: ${find_middle_of_linked_list(head).value}`);
 
 // Solution
 // -----
