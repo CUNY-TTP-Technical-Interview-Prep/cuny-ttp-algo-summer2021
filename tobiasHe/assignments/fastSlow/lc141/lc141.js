992ca1b4752c4d25bd1afb0cf807cf5d3eef8c49
Tobias He <tobiashe17@gmail.com>
2021-06-08 01:25:57 -0400

    lc141
    did lc141 and updated README

--- tobiasHe/assignments/fastSlow/lc141/lc141.js
+++ tobiasHe/assignments/fastSlow/lc141/lc141.js
@@ -3,34 +3,40 @@
 // Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
 
 class Node {
-  constructor(value, next=null){
-    this.value = value;
-    this.next = next;
-  }
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
 }
 
 const has_cycle = function(head) {
-  // TODO: Write your code here
-  return false
-}
+	// TODO: Write your code here
+	let slow = head;
+	let fast = head;
 
+	while (fast && fast.next) {
+		slow = slow.next;
+		fast = fast.next.next;
+		if (fast === slow) {
+			return true;
+		}
+	}
+	return false;
+};
 
-head = new Node(1)
-head.next = new Node(2)
-head.next.next = new Node(3)
-head.next.next.next = new Node(4)
-head.next.next.next.next = new Node(5)
-head.next.next.next.next.next = new Node(6)
-console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
 
-head.next.next.next.next.next.next = head.next.next
-console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
 
-head.next.next.next.next.next.next = head.next.next.next
-console.log(`LinkedList has cycle: ${has_cycle(head)}`)
-
-
-
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
 
 // Solution
 // -----
