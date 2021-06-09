63d83fa30b0f46d5b7269ff7caac2a5542acb657
simplisticmartin <simplisticmartin@gmail.com>
2021-06-09 17:11:34 -0400

    lc56 attempt:

--- assignments/mergeIntervals/lc57/lc57.js
+++ assignments/mergeIntervals/lc57/lc57.js
@@ -1,6 +1,8 @@
 // Problem Statement #
 
-// Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
+// Given a list of non-overlapping intervals sorted by their start time,
+//insert a given interval at the correct position and merge all necessary
+//intervals to produce a list that has only mutually exclusive intervals.
 
 class Interval {
   constructor(start, end) {
@@ -19,7 +21,7 @@ const insert = function (intervals, new_interval) {
   return merged;
 };
 
-process.stdout.write('Intervals after inserting the new interval: ');
+process.stdout.write("Intervals after inserting the new interval: ");
 let result = insert(
   [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
   new Interval(4, 6)
@@ -29,7 +31,7 @@ for (i = 0; i < result.length; i++) {
 }
 console.log();
 
-process.stdout.write('Intervals after inserting the new interval: ');
+process.stdout.write("Intervals after inserting the new interval: ");
 result = insert(
   [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
   new Interval(4, 10)
@@ -39,7 +41,7 @@ for (i = 0; i < result.length; i++) {
 }
 console.log();
 
-process.stdout.write('Intervals after inserting the new interval: ');
+process.stdout.write("Intervals after inserting the new interval: ");
 result = insert([new Interval(2, 3), new Interval(5, 7)], new Interval(1, 4));
 for (i = 0; i < result.length; i++) {
   result[i].print_interval();
--- martinLi/assignments/mergeIntervals/lc56/lc56attempt.js
+++ martinLi/assignments/mergeIntervals/lc56/lc56attempt.js
@@ -1,31 +1,6 @@
 // Problem Statement #
 
-// Given a list of intervals, merge all the overlapping intervals to
-// produce a list that has only mutually exclusive intervals.
-
-/*
-Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-Output: [[1,6],[8,10],[15,18]]
-Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
-
-Input: intervals = [[1,4],[4,5]]
-Output: [[1,5]]
-Explanation: Intervals [1,4] and [4,5] are considered overlapping.
-
-*/
-/*
-sort the intervals by start time.
-
-
-curr_start = intervals[0][0]
-curr_end  = intervals[0][1]
-for loop(i =0; i <intervals.leng; i++ )
-
-      intervals[i][0] > curr_start 
-        intervals[i][1] > curr_end
-          curr_end = intervals[i][1]
-
-*/
+// Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
 
 class Interval {
   constructor(start, end) {
@@ -38,70 +13,39 @@ class Interval {
   }
 }
 
-const merge = function (intervals) {
-  if (intervals.length < 2) return intervals;
-  merged = [];
 
-  intervals.sort((a, b) => a.start - b.start);
-
-  //Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-  let curr_start = intervals[0][0]; //1
-  let curr_end = intervals[0][1]; //3
-
-  for (let i = 1; i < intervals.length; i++) {
-    //2
-    if (intervals[i].start > curr_end) {
-      // 2> 3
-      merged.push(new Interval(curr_start, curr_end));
-      curr_start = intervals[i].start;
-      curr_end = intervals[i].end;
-    }
-    //[1,4][2,3]
-    // [1,3][2,6]
-    //ce=6 cs = 1
-    else {
-      //
-      curr_end = Math.max(curr_end, intervals[i].end);
-    }
-  }
-  merged.push(new Interval(curr_start, curr_end));
-
+const merge = function(intervals) {
+  merged = []
   // TODO: Write your code here
   return merged;
 };
 
-merged_intervals = merge([
-  new Interval(1, 4),
-  new Interval(2, 5),
-  new Interval(7, 9),
-]);
+merged_intervals = merge([new Interval(1, 4), new Interval(2, 5), new Interval(7, 9)]);
 result = "";
-for (i = 0; i < merged_intervals.length; i++) {
+for(i=0; i < merged_intervals.length; i++) {
   result += merged_intervals[i].get_interval() + " ";
 }
-console.log(`Merged intervals: ${result}`);
+console.log(`Merged intervals: ${result}`)
 
-merged_intervals = merge([
-  new Interval(6, 7),
-  new Interval(2, 4),
-  new Interval(5, 9),
-]);
-result = "";
-for (i = 0; i < merged_intervals.length; i++) {
-  result += merged_intervals[i].get_interval() + " ";
-}
-console.log(`Merged intervals: ${result}`);
 
-merged_intervals = merge([
-  new Interval(1, 4),
-  new Interval(2, 6),
-  new Interval(3, 5),
-]);
+merged_intervals = merge([new Interval(6, 7), new Interval(2, 4), new Interval(5, 9)]);
 result = "";
-for (i = 0; i < merged_intervals.length; i++) {
+for(i=0; i < merged_intervals.length; i++) {
   result += merged_intervals[i].get_interval() + " ";
 }
-console.log(`Merged intervals: ${result}`);
+console.log(`Merged intervals: ${result}`)
+
+
+merged_intervals = merge([new Interval(1, 4), new Interval(2, 6), new Interval(3, 5)]);
+result = "";
+for(i=0; i < merged_intervals.length; i++) {
+  result += merged_intervals[i].get_interval() + " ";
+}
+console.log(`Merged intervals: ${result}`)
+
+
+
+
 
 // Solution
 // -----
