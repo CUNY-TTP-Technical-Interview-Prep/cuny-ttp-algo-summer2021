9c2bc5c36859b46d0f7b0fc986bdfbf487d5dbf5
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-09 17:04:06 -0400

    edited

--- joshuaCarpentier/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.js
+++ joshuaCarpentier/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.js
@@ -1,17 +1,36 @@
 // Problem Statement #
 
-// We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
+// We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ 
+// based on their creation sequence. This means that the object 
+// with sequence number ‘3’ was created just before the object with sequence number ‘4’.
 
-// Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+// Write a function to sort the objects in-place on their creation sequence number in O(n) and
+//  without any extra space. For simplicity, let’s assume we are passed an integer array containing 
+//  only the sequence numbers, though each number is actually an object.
+
+
+/**
+ * Input: Unorder array
+ * Output: Sorted array
+ * 
+ * 
+ *
+ */
 
 const cyclic_sort = function (nums) {
   // TODO: Write your code here
+
   return nums;
 };
 
 console.log(`${cyclic_sort([3, 1, 5, 4, 2])}`);
 console.log(`${cyclic_sort([2, 6, 4, 3, 1, 5])}`);
 console.log(`${cyclic_sort([1, 5, 6, 4, 3, 2])}`);
+
+
+
+
+
 
 // Solution
 // -----
