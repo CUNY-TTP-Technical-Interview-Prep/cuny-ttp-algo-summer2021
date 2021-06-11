13c14bdbea6d2a6d4f675e85b8ec4dc21a6afe61
Jan Markus Milan <jmarkusm@me.com>
2021-06-11 19:35:57 -0400

    feat: complete cyclicSortEasy, lc442 and lc448

--- janMarkusMilan/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.js
+++ janMarkusMilan/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.js
@@ -4,14 +4,46 @@
 
 // Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
 
+/*
+Given: [3, 1, 5, 2, 4]
+Return: [1, 2, 3, 4, 5]
+Edge Case: Return [] if it is an empty array
+
+Steps:
+  1. We want to sort the values in the array by index (index 0 = 1, index 1 = 2, ...)
+     Simply put, arr[i] === i + 1
+     To do this, we should use cyclic sort
+  2. To implement, we need an increment that starts at 0 and increments conditionally
+     We should use a while loop because we only want to increment if the value is in the proper index (multiple swaps possible)
+     We initialize another variable that will serve as our swap index (arr[i] - 1)
+     We swap as long as the value is not in it's proper index and the values we swap are not the same, else we just increment
+     We finally return the array
+*/
+
 const cyclic_sort = function (nums) {
   // TODO: Write your code here
+  let startIdx = 0;
+
+  while (startIdx < nums.length) {
+    if (nums[startIdx] !== startIdx + 1) {
+      const swapIdx = nums[startIdx] - 1;
+      if (nums[startIdx] !== nums[swapIdx]) {
+        [nums[startIdx], nums[swapIdx]] = [nums[swapIdx], nums[startIdx]];
+      } else {
+        startIdx++;
+      }
+    } else {
+      startIdx++;
+    }
+  }
+
   return nums;
 };
 
 console.log(`${cyclic_sort([3, 1, 5, 4, 2])}`);
 console.log(`${cyclic_sort([2, 6, 4, 3, 1, 5])}`);
 console.log(`${cyclic_sort([1, 5, 6, 4, 3, 2])}`);
+console.log(`${cyclic_sort([])}`);
 
 // Solution
 // -----
--- janMarkusMilan/assignments/cyclicSort/lc442/lc442.js
+++ janMarkusMilan/assignments/cyclicSort/lc442/lc442.js
@@ -1,16 +1,60 @@
 // Problem Statement #
 
-// We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. The array has some numbers appearing twice, find all these duplicate numbers without using any extra space.
+// We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’.
+// The array has some numbers appearing twice, find all these duplicate numbers without using any extra space.
 
-const find_all_duplicates = function(nums) {
-  duplicateNumbers = [];
+/*
+[3, 4, 4, 5, 5]
+Sort: [x, x, 3, 4, 5] -> x = the duplicate numbers
+Return: [4, 5]
+Edge Case: return [] (if no missing num or empty arr)
+
+Steps:
+  1. Cyclic sort (all the numbers will be in the correct index)
+  2. Once I sort, compare the values on the index (index 0 -> 1, index 1 -> 2, index 2 -> 3)
+  3. The duplicate will be on a random index
+  4. Check if I have the proper value in each index
+  5. If I don't, then it's a duplicate number and just add it to the res array (arr[i] -> push into my res arr)
+  6. If I do, increment and proceed on in the array
+  7. Res arr should have all the duplicate values
+
+Starting point (incremented val)
+While loop (increment conditionally)
+If condition: check if the value matches the index
+Some val = arr[starting point] - 1
+Swap using destructuring
+Once the val is in the correct index, increment the starting value
+*/
+
+const find_all_duplicates = function (nums) {
   // TODO: Write your code here
+  duplicateNumbers = [];
+
+  let startIdx = 0;
+  while (startIdx < nums.length) {
+    if (nums[startIdx] !== startIdx + 1) {
+      const swapIdx = nums[startIdx] - 1;
+      if (nums[startIdx] !== nums[swapIdx]) {
+        [nums[startIdx], nums[swapIdx]] = [nums[swapIdx], nums[startIdx]];
+      } else {
+        startIdx++;
+      }
+    } else {
+      startIdx++;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+      duplicateNumbers.push(nums[i]);
+    }
+  }
+
   return duplicateNumbers;
 };
 
 console.log(find_all_duplicates([3, 4, 4, 5, 5]));
 console.log(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]));
-
 
 // Solution
 // -----
