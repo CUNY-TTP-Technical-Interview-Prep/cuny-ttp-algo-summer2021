7295968c8b49e7bc6dd11bffd20ddc2500c55a66
Wilson Chan <wilsonchan54@gmail.com>
2021-06-10 22:56:14 -0400

    Complete cyclicSortEasy.py assignment / update README.md

--- wilsonChan/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.py
+++ wilsonChan/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.py
@@ -1,11 +1,56 @@
 # Problem Statement #
 
-# We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
+# We are given an array containing ‘n’ objects. Each object, 
+# when created, was assigned a unique number from 1 to ‘n’ based on 
+# their creation sequence. This means that the object with sequence number 
+# ‘3’ was created just before the object with sequence number ‘4’.
 
-# Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+# Write a function to sort the objects in-place on their creation 
+# sequence number in O(n) and without any extra space. For simplicity, 
+# let’s assume we are passed an integer array containing only the 
+# sequence numbers, though each number is actually an object.
+
+'''
+
+EXAMPLE:
+[2, 6, 4, 3, 1, 5] -> input
+
+[2, 6, 4, 3, 1, 5] -> swap 2 and 6
+[6, 2, 4, 3, 1, 5] -> swap 6 and 5
+[5, 2, 4, 3, 1, 6] -> swap 5 and 1
+[1, 2, 4, 3, 5, 6] -> swap 4 and 3
+[1, 2, 3, 4, 5, 6] -> done
+
+
+STEPS:
+1) Initialize start variable
+2) While loop with the condition of start < length of array
+3) j represents the start value - 1 (this will be the next index position)
+4) If starting index != next index position
+  -swap starting index and j
+5) Otherwise increment to next index
+
+
+COMPLEXITY:
+Time: O(n)
+Space: O(1) since the size of the array doesn't change
+
+'''
+
+
 
 def cyclic_sort(nums):
   # TODO: Write your code here
+
+  start = 0
+
+  while start < len(nums):
+    j = nums[start] - 1               #value on starting index - 1 = next swap location
+    if nums[start] != nums[j]:
+      nums[start], nums[j] = nums[j], nums[start]    #swapping values
+    else:
+      start += 1
+
   return nums
 
 def main():
