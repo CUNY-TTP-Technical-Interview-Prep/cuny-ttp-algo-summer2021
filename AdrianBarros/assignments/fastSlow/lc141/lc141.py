43d8274f785f5bc05238b7a2d0793ef7031be163
Adrian Barros <adriansbarros1@gmail.com>
2021-06-13 15:57:19 -0400

    75, 713, 259, 141  done

--- AdrianBarros/assignments/fastSlow/lc141/lc141.py
+++ AdrianBarros/assignments/fastSlow/lc141/lc141.py
@@ -2,31 +2,50 @@
 
 # Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
 
+
 class Node:
-  def __init__(self, value, next=None):
-    self.value = value
-    self.next = next
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+'''
+input: a linklist
+output : bool
+
+Step 1: starts declaring two pinters, slow and fast pointing to the head
+step 2: move both of the pointers slow by 1 and fast by two
+step 3 : if they touch there is a loop
+        iff fast recheas the end there is not
+'''
 
 
 def has_cycle(head):
-  # TODO: Write your code here
-  return False
+    # TODO: Write your code here
+    slow = head
+    fast = head
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+            return True
+    return False
 
 
 def main():
-  head = Node(1)
-  head.next = Node(2)
-  head.next.next = Node(3)
-  head.next.next.next = Node(4)
-  head.next.next.next.next = Node(5)
-  head.next.next.next.next.next = Node(6)
-  print("LinkedList has cycle: " + str(has_cycle(head)))
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    print("LinkedList has cycle: " + str(has_cycle(head)))
 
-  head.next.next.next.next.next.next = head.next.next
-  print("LinkedList has cycle: " + str(has_cycle(head)))
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
 
-  head.next.next.next.next.next.next = head.next.next.next
-  print("LinkedList has cycle: " + str(has_cycle(head)))
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
 
 
 main()
@@ -43,9 +62,3 @@ main()
 #   return False
 
 # -----
-
-# Time Complexity #
-# As we have concluded above, once the slow pointer enters the cycle, the fast pointer will meet the slow pointer in the same loop. Therefore, the time complexity of our algorithm will be O(N) where ‘N’ is the total number of nodes in the LinkedList.
-
-# Space Complexity #
-# The algorithm runs in constant space O(1).
--- AdrianBarros/assignments/twopointers/lc713/lc713.py
+++ AdrianBarros/assignments/twopointers/lc713/lc713.py
@@ -4,17 +4,50 @@
 
 # input: arr[] && target
 # output:
+'''
+follow up questions:
+by definition can 1 number be a subarray?
+are we just counting the subarrays?
+'''
+'''
+because we are dealing with positive numbers we do not have to worry about diving for 0
+hence we can baically have a slidding window approach with a number and a ptr follwing it
+step 1: loop through the array 
+Step 2: for each number multiply the next one to the curr number
+        IF not what we are looking for (product < k) we will loop moving the start until either we get something or it touches the curr num
+        ELSE we add all the sub arrays into the response
+return res
+
+'''
 
 
-def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+
+
+from typing import List
+def find_subarrays(self, nums: List[int], k: int) -> int:
     res = 0
+    # initialize the start of the subarray and the product as 1 for us to multiply more numbers into it
+    start = 0
+    product = 1
     # TODO: Write your code here
+    # step 1: loop through the array
+    for i in range(len(nums)):
+        # Step 2: for each number multiply the next one to the curr number
+        product *= nums[i]
+        if product >= k:
+            while product >= k and start <= i:
+                product /= nums[start]
+                start += 1
+        # adding all the subarrays into the responce
+        res += i - start + 1
+
     return res
 
 
 def main():
-    print(find_subarrays([2, 5, 3, 10], 30))
-    print(find_subarrays([8, 2, 6, 5], 50))
+    print(find_subarrays(0, [2, 5, 3, 10], 30))  # expected 6
+    print(find_subarrays(0, [8, 2, 6, 5], 50))  # expected 7
+    print(find_subarrays(0, [1, 2, 3, 4], 50))  # expected 10
 
 
 main()
