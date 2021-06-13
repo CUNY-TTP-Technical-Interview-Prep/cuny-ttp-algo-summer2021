04d0de0527f83e5260076d511ac022a6fdd3f1ad
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-13 11:41:24 -0400

    fixed file

--- assignments/reverseLinked/lc206/lc206.js
+++ assignments/reverseLinked/lc206/lc206.js
@@ -2,23 +2,6 @@
 
 // Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
 
-/**
- * Input: head of a Singly LinkedList
- * Output: new head of the reversed LinkedList
- * 
- * Properties: head, next pointer, Node value, null, current position, next position, last position
- * 
- * Pre-Condition: if list empty or only has null
- * 
- * Conditions: while current head not equal to null 
- * 
- * let next equal next value in list then update current next node to null 
- * then null to the head and the head to next
- * 
- * 
- * 
- */
-
 class Node {
   constructor(value, next=null){
     this.value = value;
@@ -38,34 +21,15 @@ class Node {
 
 
 const reverse = function(head) {
-  if(head == null || head < 0) return null;
-  let current = head;
-  let previous = null;
-
-  while(current !== null){
-   let next = current.next;
-    current.next = previous;
-    previous = current;
-    current = next;
-  }
-
-
-  return previous;
+  // TODO: Write your code here
+  return head;
 };
 
-let head;
-// console.log(head);
 head = new Node(2);
-// console.log(head); // first node but shows whole list
 head.next = new Node(4);
 head.next.next = new Node(6);
 head.next.next.next = new Node(8);
 head.next.next.next.next = new Node(10);
-// console.log(head); // whole list
-let nullval = head.next.next.next.next.next;
-// console.log(nullval); // null
-// head = new Node();
-// console.log(head);
 
 console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
 console.log(`Nodes of reversed LinkedList are: ${reverse(head).get_list()}`)
--- joshuaCarpentier/assignments/reverseLinked/lc206/lc206.js
+++ joshuaCarpentier/assignments/reverseLinked/lc206/lc206.js
@@ -3,24 +3,31 @@
 // Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
 
 /**
- * Input:
- * Output:
+ * Input: head of a Singly LinkedList
+ * Output: new head of the reversed LinkedList
  * 
- * Properties:
+ * Properties: head, next pointer, Node value, null, current position, next position, last position
  * 
- * Conditions:
+ * Pre-Condition: if list empty or only has null
+ * 
+ * Conditions: while current head not equal to null 
+ * 
+ * let next equal next value in list then update current next node to null 
+ * then null to the head and the head to next
+ * 
+ * 
  * 
  */
 
-class Node {
+ class Node {
   constructor(value, next=null){
     this.value = value;
     this.next = next;
   }
 
   get_list() {
-    result = "";
-    temp = this;
+    let result = "";
+    let temp = this;
     while (temp !== null) {
       result += temp.value + " ";
       temp = temp.next;
@@ -31,21 +38,37 @@ class Node {
 
 
 const reverse = function(head) {
+  if(head == null || head < 0) return null;
+  let current = head;
+  let previous = null;
+
+  while(current !== null){
+   let next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
 
 
-
-  return head;
+  return previous;
 };
 
+let head;
+// console.log(head);
 head = new Node(2);
+// console.log(head); // first node but shows whole list
 head.next = new Node(4);
 head.next.next = new Node(6);
 head.next.next.next = new Node(8);
 head.next.next.next.next = new Node(10);
+// console.log(head); // whole list
+let nullval = head.next.next.next.next.next;
+// console.log(nullval); // null
+// head = new Node();
+// console.log(head);
 
 console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
 console.log(`Nodes of reversed LinkedList are: ${reverse(head).get_list()}`)
-
 
 
 // Solution
