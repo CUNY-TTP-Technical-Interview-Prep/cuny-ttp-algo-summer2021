6430fbdfef76cadea17c4f10d7b37cd0fb762623
Serda Simus <iamserda@gmail.com>
2021-06-13 19:17:49 -0400

    copied the solution, in order to catch up. I will read on the solution later.

--- serdaSimus/assignments/03FastSlowPattern/lc142/lc142.js
+++ serdaSimus/assignments/03FastSlowPattern/lc142/lc142.js
@@ -2,35 +2,78 @@
 
 // Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
 
+/*
+I still have no clue how this works exactly.
+
+*/
+
 class Node {
-  constructor(value, next=null){
+  constructor(value, next = null) {
     this.value = value;
     this.next = next;
   }
 }
 
-const find_cycle_start = function(head){
-  // TODO: Write your code here
-  return head;
-};
+function find_cycle_start(head) {
+  cycle_length = 0;
+  // find the LinkedList cycle
+  let slow = head,
+    fast = head;
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (slow === fast) {
+      // found the cycle
+      cycle_length = calculate_cycle_length(slow);
+      break;
+    }
+  }
+  return find_start(head, cycle_length);
+}
 
+function calculate_cycle_length(slow) {
+  let current = slow,
+    cycle_length = 0;
+  while (true) {
+    current = current.next;
+    cycle_length += 1;
+    if (current === slow) {
+      return cycle_length; // wouldn't be current - 1?
+    }
+  }
+}
 
-head = new Node(1)
-head.next = new Node(2)
-head.next.next = new Node(3)
-head.next.next.next = new Node(4)
-head.next.next.next.next = new Node(5)
-head.next.next.next.next.next = new Node(6)
+function find_start(head, cycle_length) {
+  let pointer1 = head,
+    pointer2 = head;
+  // move pointer2 ahead 'cycle_length' nodes
+  while (cycle_length > 0) {
+    pointer2 = pointer2.next;
+    cycle_length -= 1;
+  }
+  // increment both pointers until they meet at the start of the cycle
+  while (pointer1 !== pointer2) {
+    pointer1 = pointer1.next;
+    pointer2 = pointer2.next;
+  }
 
-head.next.next.next.next.next.next = head.next.next
-console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`)
+  return pointer1;
+}
 
-head.next.next.next.next.next.next = head.next.next.next
-console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`)
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
 
-head.next.next.next.next.next.next = head
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);
 
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);
 
+head.next.next.next.next.next.next = head;
 
 // Solution
 // -----
@@ -50,7 +93,6 @@ head.next.next.next.next.next.next = head
 //   return find_start(head, cycle_length);
 // }
 
-
 // function calculate_cycle_length(slow) {
 //   let current = slow,
 //     cycle_length = 0;
@@ -63,7 +105,6 @@ head.next.next.next.next.next.next = head
 //   }
 //   return cycle_length;
 // }
-
 
 // function find_start(head, cycle_length) {
 //   let pointer1 = head,
