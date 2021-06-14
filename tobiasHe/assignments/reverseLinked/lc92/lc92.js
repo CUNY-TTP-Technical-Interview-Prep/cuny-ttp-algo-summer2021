802202910d5b6cbe69e6e81b88c85bf6c14d7662
Tobias He <tobiashe17@gmail.com>
2021-06-14 17:14:32 -0400

    lc92
    did lc92, updated README

--- tobiasHe/assignments/reverseLinked/lc92/lc92.js
+++ tobiasHe/assignments/reverseLinked/lc92/lc92.js
@@ -3,36 +3,91 @@
 // Given the head of a LinkedList and two positions ‘p’ and ‘q’, reverse the LinkedList from position ‘p’ to ‘q’.
 
 class Node {
-  constructor(value, next=null){
-    this.value = value;
-    this.next = next;
-  }
+	constructor(value, next = null) {
+		this.value = value;
+		this.next = next;
+	}
 
-  get_list() {
-    result = "";
-    temp = this;
-    while (temp !== null) {
-      result += temp.value + " ";
-      temp = temp.next;
-    }
-    return result;
-  }
-};
+	get_list() {
+		let result = '';
+		let temp = this;
+		while (temp !== null) {
+			result += temp.value + ' ';
+			temp = temp.next;
+		}
+		return result;
+	}
+}
 
 const reverse_sub_list = function(head, p, q) {
-  // TODO: Write your code here
-  return head;
+	// TODO: Write your code here
+	// 1 -> 2 -> 3 -> 4 -> 5 -> 6
+	//      p         q
+	// 1 -> 4 -> 3 -> 2 -> 5 -> 6
+
+	// 2 3 4 5
+	// 5 4 3 2
+	// 1 5 4 3 2 6
+	// previous, next
+	// start at position p
+	// increment head.next  p - 1 steps  head = whatever it's at position p
+
+	// p = 2
+	// q = 4
+	if (p === q) return head;
+
+	let previous = null;
+	let next = null;
+	let current = head;
+
+	if (current) {
+		for (let i = 0; i < p - 1; i++) {
+			previous = current;
+			current = current.next;
+		}
+	}
+
+	let j = 0;
+	let lastNodeOfFirst = null;
+	if (previous) {
+		lastNodeOfFirst = previous; //need access to the last node of first part
+	}
+
+	tailOfSub = current;
+
+	//reverse from p to q
+	while (current && j < q - p + 1) {
+		next = current.next;
+		current.next = previous;
+		previous = current;
+		current = next;
+		j++;
+	}
+
+	console.log(current);
+
+	//connect first part
+	if (lastNodeOfFirst !== null) {
+		lastNodeOfFirst.next = previous;
+	} else {
+		head = previous;
+	}
+
+	//connect last part
+	tailOfSub.next = current;
+
+	return head;
 };
 
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
 
-head = new Node(1)
-head.next = new Node(2)
-head.next.next = new Node(3)
-head.next.next.next = new Node(4)
-head.next.next.next.next = new Node(5)
-
-console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
-console.log(`Nodes of reversed LinkedList are: ${reverse_sub_list(head, 2, 4).get_list()}`)
+console.log(`Nodes of original LinkedList are: ${head.get_list()}`);
+console.log(`Nodes of reversed LinkedList are: ${reverse_sub_list(head, 2, 4).get_list()}`);
 
 // Solution
 // -----
