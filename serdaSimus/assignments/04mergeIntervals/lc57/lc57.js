7f474f8fe177a101f07da83b467dc5ecb962c51c
Serda Simus <iamserda@gmail.com>
2021-06-13 21:29:32 -0400

    Lc57js updated notes, i have attempted to rewrite the code 3x but that's hasn't brought me any more clarity as to how to solve it alone. Let alone within 25mins. WIP.

--- serdaSimus/assignments/04mergeIntervals/lc57/lc57.js
+++ serdaSimus/assignments/04mergeIntervals/lc57/lc57.js
@@ -2,49 +2,179 @@
 
 // Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
 
+
+
+/*
+Problem Statement #
+Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
+
+Example 1:
+
+Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,6]
+Output: [[1,3], [4,7], [8,12]]
+Explanation: After insertion, since [4,6] overlaps with [5,7], we merged them into one [4,7].
+
+Example 2:
+
+Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,10]
+Output: [[1,3], [4,12]]
+Explanation: After insertion, since [4,10] overlaps with [5,7] & [8,12], we merged them into [4,12].
+
+Example 3:
+
+Input: Intervals=[[2,3],[5,7]], New Interval=[1,4]
+Output: [[1,4], [5,7]]
+Explanation: After insertion, since [1,4] overlaps with [2,3], we merged them into one [1,4].
+*/
+
+
 class Interval {
-  constructor(start, end) {
-    this.start = start;
-    this.end = end;
-  }
+	constructor(start, end) {
+		this.start = start;
+		this.end = end;
+	}
 
-  print_interval() {
-    process.stdout.write(`[${this.start}, ${this.end}]`);
-  }
+	print_interval() {
+		process.stdout.write(`[${this.start}, ${this.end}]`);
+	}
 }
+/* Solution:
+	loop intervals:
+		check if newElem.start is less than currElem.start
+			update current elem. start.
+		check if newElem. is less than current.end
+			check if new.end is less/equal to current.end
+      
 
-const insert = function (intervals, new_interval) {
-  let merged = [];
-  // TODO: Write your code here
-  return merged;
+      *** I am still trying to wrap my head around this.
+      I have started reading up on the solution and leetcode.
+      when i have my own solution I will update this.
+      The approach of just rewriting the code hasn't done much for me.
+      reverse engineering is taking a bit long.
+      
+*/
+const insert = function(intervals, new_interval) {
+	let merged = [];
+	// TODO: Write your code here
+	for(let index=0; index<intervals.length; index++){
+		let current = intervals[index]
+		let start, end;
+		if(new_interval.start > current.end){
+			merged.push(current)
+		}else{
+			if(new_interval.start <= current.start){
+				// it means new Interval covers this
+				let start = 
+			}else{
+        // 
+			} 
+		}
+
+	}
+
+	return merged;
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Test Cases:
 process.stdout.write('Intervals after inserting the new interval: ');
-let result = insert(
-  [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
-  new Interval(4, 6)
-);
+let result = insert([
+	new Interval(1, 3),
+	new Interval(5, 7),
+	new Interval(8, 12),
+], new Interval(4, 6));
 for (i = 0; i < result.length; i++) {
-  result[i].print_interval();
+	result[i].print_interval();
 }
 console.log();
 
 process.stdout.write('Intervals after inserting the new interval: ');
-result = insert(
-  [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
-  new Interval(4, 10)
-);
+result = insert([
+	new Interval(1, 3),
+	new Interval(5, 7),
+	new Interval(8, 12),
+], new Interval(4, 10));
 for (i = 0; i < result.length; i++) {
-  result[i].print_interval();
+	result[i].print_interval();
 }
 console.log();
 
 process.stdout.write('Intervals after inserting the new interval: ');
-result = insert([new Interval(2, 3), new Interval(5, 7)], new Interval(1, 4));
+result = insert([new Interval(2, 3),
+new Interval(5, 7),
+], new Interval(1, 4));
 for (i = 0; i < result.length; i++) {
-  result[i].print_interval();
+	result[i].print_interval();
 }
 console.log();
+
+
+
+// class Interval {
+//   constructor(start, end) {
+//     this.start = start;
+//     this.end = end;
+//   }
+
+//   print_interval() {
+//     process.stdout.write(`[${this.start}, ${this.end}]`);
+//   }
+// }
+
+// const insert = function (intervals, new_interval) {
+//   let merged = [];
+//   // TODO: Write your code here
+
+//   return merged;
+// };
+
+// process.stdout.write('Intervals after inserting the new interval: ');
+// let result = insert(
+//   [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
+//   new Interval(4, 6)
+// );
+// for (i = 0; i < result.length; i++) {
+//   result[i].print_interval();
+// }
+// console.log();
+
+// process.stdout.write('Intervals after inserting the new interval: ');
+// result = insert(
+//   [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
+//   new Interval(4, 10)
+// );
+// for (i = 0; i < result.length; i++) {
+//   result[i].print_interval();
+// }
+// console.log();
+
+// process.stdout.write('Intervals after inserting the new interval: ');
+// result = insert([new Interval(2, 3), new Interval(5, 7)], new Interval(1, 4));
+// for (i = 0; i < result.length; i++) {
+//   result[i].print_interval();
+// }
+// console.log();
 
 // Solution
 // -----
