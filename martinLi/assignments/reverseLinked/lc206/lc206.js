4fc89c51d4bbde862c309a1dc5e2bc2502770e6c
simplisticmartin <simplisticmartin@gmail.com>
2021-06-15 00:09:40 -0400

    leetcode 206 actual previous was 448

--- martinLi/assignments/reverseLinked/lc206/lc206.js
+++ martinLi/assignments/reverseLinked/lc206/lc206.js
@@ -3,7 +3,7 @@
 // Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
 
 class Node {
-  constructor(value, next=null){
+  constructor(value, next = null) {
     this.value = value;
     this.next = next;
   }
@@ -17,10 +17,38 @@ class Node {
     }
     return result;
   }
-};
+}
 
-
-const reverse = function(head) {
+const reverse = function (head) {
+  let prev = null;
+  /**
+   * [2] -> [4] -> [6] -> [8] -> [10] -> null
+   *                                    p
+   *  c
+   *         n
+   *  p
+   *         c
+   *                n
+   *
+   */
+  let current = head;
+  while (current != null) {
+    /**
+     * while current node isnt null
+     * we set next to the current next
+     * and we set the current next to prev
+     * current's next is pointed to the node previously
+     *
+     * current is set to next until we hit null
+     *
+     * We then return previous
+     */
+    next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return previous;
   // TODO: Write your code here
   return head;
 };
@@ -31,10 +59,8 @@ head.next.next = new Node(6);
 head.next.next.next = new Node(8);
 head.next.next.next.next = new Node(10);
 
-console.log(`Nodes of original LinkedList are: ${head.get_list()}`)
-console.log(`Nodes of reversed LinkedList are: ${reverse(head).get_list()}`)
-
-
+console.log(`Nodes of original LinkedList are: ${head.get_list()}`);
+console.log(`Nodes of reversed LinkedList are: ${reverse(head).get_list()}`);
 
 // Solution
 // -----
