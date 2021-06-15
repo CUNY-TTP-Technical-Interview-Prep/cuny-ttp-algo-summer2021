55f1e869edec1ae1abe7afc222aa541d932fac25
Adrian Barros <adriansbarros1@gmail.com>
2021-06-14 22:06:13 -0400

    92 986  done

--- AdrianBarros/assignments/mergeIntervals/lc986/lc986.py
+++ AdrianBarros/assignments/mergeIntervals/lc986/lc986.py
@@ -1,21 +1,56 @@
 # Problem Statement #
 
 # Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
+'''
+input : two separate arrays of intervals
+output : the points where they intersect
+
+step 1: first we have to traverse both arrays
+step 2: check if any of them intersect
+step 3: store that intersection into the result
+added step: we update the smallest value of the second values
+Step 4: repeat until we do not have any other intersection
+
+final return all the intersections
+'''
+
 
 def merge(intervals_a, intervals_b):
-  result = []
-  # TODO: Write your code here
-  return result
+    result = []
+    # TODO: Write your code here
+    ptr_a = ptr_b = 0
+    first = 0
+    second = 1
+    # step 1: first we have to traverse both arrays
+    while ptr_a < len(intervals_a) and ptr_b < len(intervals_b):
+        # step 2: check if any of them intersect
+        a_overlap_b = intervals_a[ptr_a][first] >= intervals_b[ptr_b][
+            first] and intervals_a[ptr_a][first] <= intervals_b[ptr_b][second]
+    # we check Exactly the same way to b
+        b_overlap_a = intervals_b[ptr_b][first] >= intervals_a[ptr_a][
+            first] and intervals_b[ptr_b][first] <= intervals_a[ptr_a][second]
+        # if any of these is true we will store the intersection
+        # else continue
+        if (a_overlap_b or b_overlap_a):
+            # step 3: store that intersection into the result
+            result.append([max(intervals_a[ptr_a][first], intervals_b[ptr_b][first]), min(
+                intervals_a[ptr_a][second], intervals_b[ptr_b][second])])
+        if intervals_a[ptr_a][second] < intervals_b[ptr_b][second]:
+            ptr_a += 1
+        else:
+            ptr_b += 1
+
+    return result
 
 
 def main():
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]])))
-  print("Intervals Intersection: " + str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
+    print("Intervals Intersection: " +
+          str(merge([[1, 3], [5, 6], [7, 9]], [[2, 3], [5, 7]])))
+    print("Intervals Intersection: " +
+          str(merge([[1, 3], [5, 7], [9, 12]], [[5, 10]])))
 
 
 main()
-
-
 
 
 # Solution
@@ -49,7 +84,7 @@ main()
 # -----
 
 # Time complexity #
-# As we are iterating through both the lists once, the time complexity of the above algorithm is O(N + M), where ‘N’ and ‘M’ are the total number of intervals in the input arrays respectively.
+# As we are iterating through both the lists once, the time complexity of the above algorithm is O(N + M), where N and M are the total number of intervals in the input arrays respectively.
 
 # Space complexity #
 # Ignoring the space needed for the result list, the algorithm runs in constant space O(1).
