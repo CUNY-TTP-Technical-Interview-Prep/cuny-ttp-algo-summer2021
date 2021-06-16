138361e52be1185f0c550695ab02c44e87d0e6ff
kwu25 <kelvin.wu125@gmail.com>
2021-06-16 09:39:42 -0400

    Completed lc104 and lc111

--- kelvinWu/treesBFS/lc104/lc104.js
+++ kelvinWu/treesBFS/lc104/lc104.js
@@ -10,13 +10,32 @@ class TreeNode {
   }
 }
 
+//do bfs on tree
+//when looping htrough each level, add 1
+//add nodes of each nodes at current level to queue
+//repeat until go through all levels of tree
 
 function find_maximum_depth(root) {
   //Write Code Here
-  return -1
+  if (root === null) return -1;
+
+  let depth = -1;
+  let queue = [root];
+  while (queue.length > 0) {
+    let levelSize = queue.length;
+    for (let i = 0; i < levelSize; i++) {
+      let currentNode = queue.shift();
+      if (currentNode.left !== null) queue.push(currentNode.left);
+      if (currentNode.right !== null) queue.push(currentNode.right);
+    }
+    depth++;
+  }
+  return depth;
 }
-
-
+//      12
+//   7    1
+// 9    10   5
+//     11
 const root = new TreeNode(12);
 root.left = new TreeNode(7);
 root.right = new TreeNode(1);
