67b854765ecc551c3b752440ec01ea998c9007d1
ryan4summer <ryan4summer0527@gmail.com>
2021-06-16 10:45:14 -0400

    feat: finish lc117/lc117.js question

--- tiantianXia/assignments/treesBFS/lc117/lc117.js
+++ tiantianXia/assignments/treesBFS/lc117/lc117.js
@@ -33,17 +33,66 @@ class TreeNode {
   }
 };
 
+/**
+ * inputs: 
+ * @param {*} root the root of the binary tree;
+ * 
+ * step 1: loop through all the nodes in the binary tree and find current levle size of the tree level
+ * step 2: loop through all the nodes in the current level and if previous node is not null connect current node to the previous.next
+ * step 3: push left and right child to the queue if it's last node of the current levle connect current.next to null
+ * 
+ * variable queue: store all the current node, previousNode: should store the previous node in the each level
+ * 
+ * loop through all the node in the queue if queue is not empty
+ *  setup the levleSize of the current level
+ *  setup previous to null
+ *    loop through all the node in the current level
+ *       set the node to the current node
+ *        find if the previous node is not null connect previous.next to the current
+ *        if the current node is the last node of the current level connect current.next to null
+ *         push left and right child to queue
+ * 
+ * edge cases: root is null => return root;
+ */
+
 const connect_level_order_siblings = function(root) {
   // TODO: Write your code here
+  if(root === null) return root;
+
+  let queue = [root];
+
+  while(queue.length > 0){
+    let levelSize = queue.length;
+    let previousNode = null;
+
+    for(let i = 0; i < levelSize; i ++){
+      let currentNode = queue.shift();
+
+      if(previousNode !== null) previousNode.next = currentNode;
+
+      if(currentNode.left !== null) queue.push(currentNode.left);
+      if(currentNode.right !== null) queue.push(currentNode.right);
+
+      previousNode = currentNode;
+    }
+  }
+
+  return root;
 };
 
-
+/**
+ *       12
+ *     7    1
+ *   9    10 5
+ */
 var root = new TreeNode(12);
 root.left = new TreeNode(7);
 root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
-connect_level_order_siblings(root);
+connect_level_order_siblings(root); // 
 
 root.print_level_order()
+
+
