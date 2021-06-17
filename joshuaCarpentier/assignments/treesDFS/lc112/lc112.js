1c3691e91f84eda96e6e5410d6a03e074629468c
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-17 13:18:13 -0400

    edit

--- joshuaCarpentier/assignments/treesDFS/lc112/lc112.js
+++ joshuaCarpentier/assignments/treesDFS/lc112/lc112.js
@@ -1,31 +1,30 @@
 // Problem Statement #
 
-// Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum 
+// Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum
 // of all the node values of that path equals ‘S’.
 
 class TreeNode {
-
   constructor(value) {
     this.value = value;
     this.left = null;
     this.right = null;
   }
-};
+}
 
 /**
  * 
  * Input: tree
- * Output: the maximum depth level
+ * Output: Boolean value weather the totalSum of a path equals the targetSum
  * 
- * Variables: root, root. left, root.right, index, array to store the root 
+ * Variables: root, root. left, root.right, index, array to store the root essentially a stack, TotalSum
  * 
  * EdgeCase different condition for different inputs
  * 
  * Brute force Approach
  * --------------- 
- * Pre-Condition: check if empty 
+ * Pre-Condition: check if empty && if root. value = targetSum
  * 
- * Conditions: there is only really a pre condition since were only looking for the shortest path to root
+ * Conditions: 
  * 
  * Termination-Conditions: check if left and right are null
 
@@ -48,17 +47,43 @@ class TreeNode {
  * Post-Conditions: 
  * 
  * 
- */ has_path = function(root, sum) {
-  // TODO: Write your code here
+ */
+
+const has_path = (root, targetSum) => {
+  if (root === null) {
+    return false
+  }
+  
+  let nodeStack = [ root ];
+  let sumStack = [ targetSum - root.val ];
+  
+  while (nodeStack.length) {
+      let currNode = nodeStack.pop();
+      let currSum = sumStack.pop();
+      
+      if ((!currNode.left && !currNode.right) && currSum === 0) {
+          return true;
+      };
+      
+      if (currNode.left) {
+          nodeStack.push(currNode.left);
+          sumStack.push(currSum - currNode.left.val)
+      };
+      
+      if (currNode.right) {
+          nodeStack.push(currNode.right);
+          sumStack.push(currSum - currNode.right.val)
+      };
+  };
+  
   return false;
 };
 
-
-var root = new TreeNode(12)
-root.left = new TreeNode(7)
-root.right = new TreeNode(1)
-root.left.left = new TreeNode(9)
-root.right.left = new TreeNode(10)
-root.right.right = new TreeNode(5)
-console.log(`Tree has path: ${has_path(root, 23)}`)
-console.log(`Tree has path: ${has_path(root, 16)}`)
+var root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+console.log(`Tree has path: ${has_path(root, 23)}`);
+console.log(`Tree has path: ${has_path(root, 16)}`);
