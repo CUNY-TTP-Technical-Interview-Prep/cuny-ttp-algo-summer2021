86e0c9e4a79c9bed3be86e2ea517b8258c9755af
Alex Tan <AlexTAN331@outlook.com>
2021-06-17 15:56:27 -0400

    feat: finish lc #113 and #257 (from LeetCode)

--- taojieTan/assignments/treesDFS/lc113/lc113.py
+++ taojieTan/assignments/treesDFS/lc113/lc113.py
@@ -12,6 +12,27 @@
 #  / |   | \
 # 4  5   2  7
 
+'''
+  edge case: empty tree => return []
+
+  Overview:
+  We can apply DFS pattern using stack (we use list) to find all root-to-leaf paths that are equal to target sum.
+  Since leaf doesn't have left and right children, we can if condition to check if its left and right nodes are None, and the path sum is equal to target sum, 
+  if yes, we store the path to final list; 
+  else, we store its left or right node to stack, and keep repeating the steps until we traverse through every node. (when stack is empty).
+
+  pseudo code:
+  First declare and initialize a stack with (root node, target sum - root value, and sublist with root value), 
+  and a empty result list to store paths that sum up tp target sum.
+  while stack is not empty, 
+  we pop the top value from stack, 
+  and check if left and right nodes are None, and the path sum is equal to target sum,
+  if yes, we append the sublist with path nodes into result list;
+  else, we push the left node or right node, target sum - path sum, and sublist path node to stack.
+
+  When finish the while loop, we will get the final result.
+'''
+
 class TreeNode:
   def __init__(self, val, left=None, right=None):
     self.val = val
@@ -19,13 +40,34 @@ class TreeNode:
     self.right = right
 
 
-def find_paths(root, sum):
+def find_paths(root, target_sum):
+  if root is None:
+    return []
+
   allPaths = []
-  # TODO: Write your code here
+  node_stack = [(root, 0, [])]
+
+  while len(node_stack):
+    node, path_sum, path = node_stack.pop()
+
+    if node.left is None and node.right is None and path_sum + node.val == target_sum:
+      path.append(node.val)
+      allPaths.append(path)
+    
+    if node.left:
+      node_stack.append((node.left, path_sum + node.val, path + [node.val]))
+
+    if node.right:
+      node_stack.append((node.right, path_sum + node.val, path + [node.val]))
+
   return allPaths
 
 
 def main():
+  
+  root1 = None
+  sum = 23
+  print("Tree paths with sum " + str(sum) + ": " + str(find_paths(root1, sum)))
 
   root = TreeNode(12)
   root.left = TreeNode(7)
@@ -36,6 +78,12 @@ def main():
   sum = 23
   print("Tree paths with sum " + str(sum) +
         ": " + str(find_paths(root, sum)))
+
+'''
+            12
+          7    1
+        4    10  5    => return [[12, 7, 4], [12, 1, 10]]
+'''
 
 
 main()
