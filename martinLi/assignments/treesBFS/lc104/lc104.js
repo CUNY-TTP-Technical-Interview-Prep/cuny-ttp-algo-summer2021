a0cab1b05845a1746de6b4741a6336e63e04042c
simplisticmartin <simplisticmartin@gmail.com>
2021-06-16 22:25:33 -0400

    104

--- martinLi/assignments/treesBFS/lc104/lc104.js
+++ martinLi/assignments/treesBFS/lc104/lc104.js
@@ -33,8 +33,21 @@ class TreeNode {
  * Create depth_counter
  *
  * As long as there are nodes in queue
- * increment depth
+ * increment depth_counter
+ *  set length of queue length
  *
+ *  loop from 0 to length of queue
+ *    we check if there is a left or right node.
+ *    If tthere is we push the left/right node into queue
+ *
+ *  We splice/remove the number of nodes within current
+ * list
+ *
+ * return depth counter
+ *
+ *
+ *
+ * LC
  */
 
 function find_maximum_depth(root) {
@@ -65,3 +78,22 @@ console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`);
 root.left.left = new TreeNode(9);
 root.right.left.left = new TreeNode(11);
 console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`);
+
+// function find_maximum_depth(root) {
+//   if (!root) return 0;
+//   const queue = [root];
+//   let depth = 0;
+//   while (queue.length !== 0) {
+//     depth++;
+//     const len = queue.length;
+//     for (let i = 0; i < len; i++) {
+//       if (queue[i].left) queue.push(queue[i].left);
+//       if (queue[i].right) queue.push(queue[i].right);
+//     }
+//     queue.splice(0, len);
+//   }
+//   return depth;
+
+//   //Write Code Here
+//   // return -1;
+// }
