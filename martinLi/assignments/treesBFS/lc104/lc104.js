7ca6060200705882c25cfcb2133d95346cba5a84
simplisticmartin <simplisticmartin@gmail.com>
2021-06-16 21:58:36 -0400

    104

--- martinLi/assignments/treesBFS/lc104/lc104.js
+++ martinLi/assignments/treesBFS/lc104/lc104.js
@@ -1,6 +1,7 @@
 // Problem#
 
-// Given a binary tree, find its maximum depth (or height).
+// Given a binary tree, find its maximum depth
+//(or height).
 
 class TreeNode {
   constructor(val) {
@@ -10,12 +11,50 @@ class TreeNode {
   }
 }
 
+/**
+ *
+ * Algorithm highlevel overview
+ * **********Tree**************************
+ *  *        [12]
+ *      [7]       [1]
+ *     -  -   [10]    [5]
+ *
+ * q(queue) = [12]=> [12 7 1] => [7 1 ]
+ * => [7 1 10 5] => [1 10 5]
+ *
+ * qL=1
+ * L=1 => 2
+ * d=1 => 2 => 3
+ *
+ * if root does not exist / does not contain nodes
+ * return 0
+ *
+ * Create a queue containing the root
+ * Create depth_counter
+ *
+ * As long as there are nodes in queue
+ * increment depth
+ *
+ */
 
 function find_maximum_depth(root) {
-  //Write Code Here
-  return -1
-}
+  if (!root) return 0;
+  const queue = [root];
+  let depth = 0;
+  while (queue.length !== 0) {
+    depth++;
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      if (queue[i].left) queue.push(queue[i].left);
+      if (queue[i].right) queue.push(queue[i].right);
+    }
+    queue.splice(0, len);
+  }
+  return depth;
 
+  //Write Code Here
+  // return -1;
+}
 
 const root = new TreeNode(12);
 root.left = new TreeNode(7);
