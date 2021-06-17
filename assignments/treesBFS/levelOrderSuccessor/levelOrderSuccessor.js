57422ddb2df4995076a5f820e5f8011b73a1b916
hpuma <37905046+hpuma@users.noreply.github.com>
2021-06-17 10:12:32 -0400

    Update levelOrderSuccessor.js

--- assignments/treesBFS/levelOrderSuccessor/levelOrderSuccessor.js
+++ assignments/treesBFS/levelOrderSuccessor/levelOrderSuccessor.js
@@ -1,4 +1,4 @@
-/ Problem Statement #
+// Problem Statement #
 
 // Given a binary tree and a node, find the level order successor of the given node in the tree. The level order successor is the node that appears right after the given node in the level order traversal.
 
@@ -49,3 +49,4 @@ if (result != null)
 result = find_successor(root, 9)
 if (result != null)
   console.log(result.val)
+-
