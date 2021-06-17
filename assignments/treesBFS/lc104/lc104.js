4c5f6d351d0e7b79cf01bfb0b4bd3f6d2c71912e
hpuma <pumahenry@live.com>
2021-06-17 09:34:22 -0400

    Added working solution with General Idea

--- assignments/treesBFS/lc104/lc104.js
+++ assignments/treesBFS/lc104/lc104.js
@@ -9,20 +9,62 @@ class TreeNode {
     this.right = null;
   }
 }
+/*
+Precondition:
+  root: the root of the binary tree, If it can be null then handle the edge where we return -1
 
+Postcondition: returns an integer that represents the binary tree's height
+
+  nextLevel: We store the future nodes that need to be checked on a level
+  nextLevelLength: the current length of the array that is going to hold all the children
+
+  treeHeight: responsible for computing the tree height
+  currentNode: the current node that we are looking at
+
+  General Idea:
+    Add the node children to the nextLevelArray,
+    Loop the nextLevelArray until it is empty,
+    While we look at each level we keep track of the current height of the tree,
+    After we finish looking at all the children, we just return the treeheight for the binary tree
+
+ */
 
 function find_maximum_depth(root) {
-  //Write Code Here
-  return -1
+
+  if (!root) return -1;
+
+  const nextLevel = [root];
+  let nextLevelLength = 0, treeHeight = 0, currentNode = null;
+
+  while(nextLevel.length){ // Up until the tree height
+    nextLevelLength = nextLevel.length; // 1
+    for (let i = 0; i < nextLevelLength; i++){ // All the nodes in the current level while also adding nodes for the next level
+      currentNode = nextLevel.shift(); 
+      if (currentNode.left) nextLevel.push(currentNode.left);
+      if (currentNode.right) nextLevel.push(currentNode.right);
+    }
+    treeHeight++;
+  }
+  // N: number of nodes
+  // Runtime: O(n)
+  // Space complexity: O(n)
+  return treeHeight;
 }
 
+/*              treeHeight = 0, nextLevel = [root]
+        12                   => nextLevel[7, 1]       treeHeight = 1
+    7       1                 => nextLevel[9, 10, 5]  treeHeight = 2
+  9       10   5              => nextLevel[11]        treeHeight = 3
+        11                    => nextLevel[   ]       treeHeight = 4
+                          return treeHeight = 4
+ */
 
 const root = new TreeNode(12);
 root.left = new TreeNode(7);
 root.right = new TreeNode(1);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
-console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`);
+console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`); // Actual: 3, Output: 3
 root.left.left = new TreeNode(9);
 root.right.left.left = new TreeNode(11);
-console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`);
+console.log(`Tree Maximum Depth: ${find_maximum_depth(root)}`); // Actual: 4, Output: 4
