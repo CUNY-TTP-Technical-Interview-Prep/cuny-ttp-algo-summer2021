d14e7b6d98a16ae547ca8af11d7dc527b8d07c6c
Alex Tan <AlexTAN331@outlook.com>
2021-06-19 19:55:48 -0400

    feat: finish lc #442 and mark it as completed

--- taojieTan/README.md
+++ taojieTan/README.md
@@ -149,7 +149,7 @@ We will start with a brief introduction of each pattern before jumping into the 
   - [x] Find the Missing Number (easy): LC #268
   - [x] Find all Missing Numbers (easy): LC #448
   - [x] Find the Duplicate Number (easy): LC #287
-  - [] Find all Duplicate Numbers (easy): LC #442
+  - [x] Find all Duplicate Numbers (easy): LC #442
 - Intro to "In Place Reversal of a LL" Pattern: Lecture & Discussion
   - [x] Reverse a LinkedList (easy): LC #206
 
--- taojieTan/assignments/cyclicSort/lc442/lc442.py
+++ taojieTan/assignments/cyclicSort/lc442/lc442.py
@@ -2,15 +2,45 @@
 
 # We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. The array has some numbers appearing twice, find all these duplicate numbers without using any extra space.
 
+# input: an unsorted integer array range from 1 to n
+# output: an array with all duplicate numbers (appears twice)
+# edge case: one element => return []
+
+'''
+  We can apply swapping to set values in its correct place in the array and find those that are not in correct place as duplicate numbers.
+
+  First we use a while loop to traverse through the arrray, 
+  check if current value is equal to the value on the index of current value (if nums[index] is equal to nums[nums[index]-1])
+  if not, we swap these two values;
+  else, we increment the index by one to keep traversing the array.
+
+  When finish the while loop, we find values that are not equal to current index + 1 (if nums[i] != i + 1, append (nums[i]) )
+
+'''
+
 def find_all_duplicates(nums):
   duplicateNumbers = []
-  # TODO: Write your code here
+  index = 0
+
+  while index < len(nums):
+    temp = nums[index] 
+
+    if temp != nums[temp - 1]:
+      nums[index], nums[temp - 1] = nums[temp - 1], nums[index]
+    else:
+      index += 1
+  
+  for i in range(len(nums)):
+    if nums[i] != i + 1:
+      duplicateNumbers.append(nums[i])
+
+
   return duplicateNumbers
 
 def main():
   print(find_all_duplicates([3, 4, 4, 5, 5]))
   print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
-
+  print(find_all_duplicates([1]))
 
 main()
 
