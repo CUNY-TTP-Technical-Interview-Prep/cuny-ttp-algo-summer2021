5471fff26de9542ca599e4163a8eeb60c1b51df4
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-20 11:14:22 -0400

    completed but confused about line 48 & 49

--- joshuaCarpentier/assignments/treesDFS/lc112/lc112.js
+++ joshuaCarpentier/assignments/treesDFS/lc112/lc112.js
@@ -12,55 +12,48 @@ class TreeNode {
 }
 
 /**
- * 
+ *
  * Input: tree
  * Output: Boolean value weather the totalSum of a path equals the targetSum
- *  
+ *
  * Brute force Approach
- * --------------- 
+ * ---------------
  * Variables: root, root. left, root.right, index, array to store the root essentially a stack, TotalSum
- * 
- * 
+ *
+ * dfs and preorder
+ *
  */
-
 
 /**
  *  Optimal Approach
  * -----------------
  *  Variables: root, root. left, root.right, index, array to store the root essentially a stack, TotalSum
- * 
- * 
+ *
+ *
  */
 
-var has_path = function (root, sum) {
-  return dfs(root, 0, sum);
+// closure version which is used for data encapulation and uses more memory 
+// recursition proably makes more sense then iterative since were dealing with tree data structure
+// in this case for dfs we wouldnt need to use a stack 
+const has_path = (root, targetSum) => {
+  // Pre-Condition: check if empty && if root.value = targetSum
+  if (!root) return false;
+  if (root.value === targetSum) return true;
+
+  let truthChecker = false; // storing state
+  const dfs = (node, sum = 0) => {
+    sum += node.value;
+    if (!node.left && !node.right && sum === targetSum) truthChecker = true; // base Case
+    // Termination-Conditions: check left and right are path sum
+    node.left && dfs(node.left, sum) // traverse left side ? 
+    node.right && dfs(node.right, sum);
+  };
+  dfs(root);
+
+  return truthChecker;
 };
 
-var dfs = function (curr, currentSum, targetSum) {
-   // Pre-Condition: check if empty && if root. value = targetSum
-  
- // Conditions: 
-  
- // Termination-Conditions: check if left and right are null
-
- // Post-Conditions: 
-  if (!curr) {
-    return false;
-  }
-
-  currentSum += curr.value;
-
-  if (curr.left === null && curr.right === null) {
-    return currentSum === targetSum;
-  }
-
-  return (
-    dfs(curr.left, currentSum, targetSum) ||
-    dfs(curr.right, currentSum, targetSum)
-  );
-};
-
-var root = new TreeNode(12);
+let root = new TreeNode(12);
 root.left = new TreeNode(7);
 root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
@@ -70,4 +63,5 @@ console.log(`Tree has path: ${has_path(root, 23)}`);
 console.log(`Tree has path: ${has_path(root, 16)}`);
 
 console.log(`\n ------- Edge Case -------- `);
-console.log(`Tree has path: ${has_path(root, 16)}`);
+console.log(`Tree has path: ${has_path(root, 0)}`);
+console.log(`Tree has path: ${has_path(root, root.value)}`);
