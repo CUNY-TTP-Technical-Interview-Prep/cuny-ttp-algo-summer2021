1390e688cd2ad132f783d55a8389e2863812bcb3
hpuma <pumahenry@live.com>
2021-06-22 11:27:36 -0400

    Added comments to LC112, ready for PR

--- henryPuma/assignments/treesDFS/lc112/lc112.js
+++ henryPuma/assignments/treesDFS/lc112/lc112.js
@@ -10,6 +10,7 @@ class TreeNode {
     this.right = null;
   }
 };
+
 /*
 Precondition: 
 root: The root of the binary tree, if null automatically return false
@@ -25,6 +26,23 @@ nodeStack: Keeps track of all the future nodes that we are going to look at,
 sumStack: Keep the compliment sum of the current node and the targetSum <=> sum (parameter)
 
 LEAF: A node where node !== null, node.left and node.right === null
+
+
+General Idea:
+Keep a nodeStack list that keeps track of the nodes being processed,
+Keep a sumStack list that keeps track of the overall sum of the nodes that have been processed,
+Have 2 variables: currentNode and currentSum that temporarily hold the popped values from nodeStack and sumStack,
+currentNode: Takes the next node for processing,
+currentSum: Takes the overall sum of the nodes processed,
+
+LINE: 52 Check if the current node is a leaf, if it is ==> check if the currentSum === 0 meaning we've subtracted node values that add up to sum ("Target sum")
+If this case is met then return TRUE
+
+Otherwise, check the currentNode's children (left && right),
+If either exist, then add that node to the nodeStack and update the sumStack with currentSum where (currentSum- currentNode.child.value) gets pushed to the sum stack
+
+
+If for some reason we process all the paths in the binary tree and NO PATH adds up to sum, then by default we return false
 
 */
 
