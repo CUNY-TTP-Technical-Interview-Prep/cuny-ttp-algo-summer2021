9561281d992802523ac9933a8b0baa7f556c47aa
jacgit18 <jacdevgoo2020@gmail.com>
2021-06-23 18:19:15 -0400

    Completed  295

--- joshuaCarpentier/assignments/heaps/lc295/lc295.js
+++ joshuaCarpentier/assignments/heaps/lc295/lc295.js
@@ -7,16 +7,34 @@
 
 // If the count of numbers inserted in the class is even, the median will be the average of the middle two numbers.
 
-class MedianOfAStream {
-  insert_num(num) {
-    // TODO: Write your code here
-    return -1;
-  }
 
-  find_median(self) {
-    // TODO: Write your code here
-    return 0.0;
-  }
+
+  /**
+   * 
+   * Input: 
+   * Output: 
+   *  
+   * Naive Approach
+   * --------------- 
+   * Variables: 
+   * 
+   * 
+   */
+class MedianOfAStream {
+  constructor() {
+    this.a = [];
+}
+
+insert_num(num) {
+    this.a.push(num);
+}
+
+find_median() {
+    this.a.sort((x, y) => x - y);
+    let n = this.a.length;
+    let m = n >> 1;
+    return n & 1 ? this.a[m] : (this.a[m - 1] + this.a[m]) / 2;
+}
 }
 
 var medianOfAStream = new MedianOfAStream();
@@ -27,3 +45,17 @@ medianOfAStream.insert_num(5);
 console.log(`The median is: ${medianOfAStream.find_median()}`);
 medianOfAStream.insert_num(4);
 console.log(`The median is: ${medianOfAStream.find_median()}`);
+
+console.log(medianOfAStream)
+console.log(`\n ------- Edge Cases ----------`)
+
+
+
+  /**
+   *  
+   * Optimal Approach
+   * --------------- 
+   * Variables: 
+   * 
+   * 
+   */
