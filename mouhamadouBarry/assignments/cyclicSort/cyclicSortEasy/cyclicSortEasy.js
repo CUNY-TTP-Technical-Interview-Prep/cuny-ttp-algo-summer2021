9862b813bd339537d54d026f308f52c67e5e5979
grimlock123 <barry.mouhamadou6@gmail.com>
2021-06-24 11:27:04 -0400

    q1 finished

--- mouhamadouBarry/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.js
+++ mouhamadouBarry/assignments/cyclicSort/cyclicSortEasy/cyclicSortEasy.js
@@ -1,17 +1,66 @@
 // Problem Statement #
 
-// We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
+// We are given an array containing ‘n’ objects. Each object, when created, was assigned a unique number from 1 to ‘n’ based on their creation sequence. 
+//This means that the object with sequence number ‘3’ was created just before the object with sequence number ‘4’.
 
-// Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
+// Write a function to sort the objects in-place on their creation sequence number in O(n) and without any extra space. 
+//For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, 
+//though each number is actually an object.
 
+
+
+/*
+INPUT: 
+  - An array of positive ints
+
+OUTPUT:
+  - Sort the array in place base on their sequence numbers
+
+Overview:
+  - start at 0 
+  - use a while(true)
+    ->check if I is in the right place
+  - temp variable
+
+
+
+  [3,2,4,1]
+
+  [[3], 2, 4, 1]
+
+  [1, 2, 4, [3]]
+
+  [1, 2, 3, 4]
+
+*/
 const cyclic_sort = function (nums) {
   // TODO: Write your code here
+
+  let count = 0
+  let temp = 0
+  while ( count < nums.length){
+    
+   // for(let i=0;i<nums.length;i++){
+      
+      if (nums[count]!==(count+1)){
+        
+        temp = nums[count]
+        nums[count] = nums[temp-1]
+        nums[temp-1] = temp
+
+      // }
+    } else { 
+      count++ 
+    } 
+  }
+
   return nums;
 };
 
 console.log(`${cyclic_sort([3, 1, 5, 4, 2])}`);
 console.log(`${cyclic_sort([2, 6, 4, 3, 1, 5])}`);
 console.log(`${cyclic_sort([1, 5, 6, 4, 3, 2])}`);
+console.log(`${cyclic_sort([1, 5, 6, 6, 3, 2])}`);
 
 // Solution
 // -----
