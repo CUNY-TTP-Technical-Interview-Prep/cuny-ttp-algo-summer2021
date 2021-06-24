3ab833dbef0bc5f706bcbdf49370733650eeafe2
kwu25 <kelvin.wu125@gmail.com>
2021-06-24 12:34:30 -0400

    Added subset problems. Completed lc442

--- kelvinWu/cyclicSort/lc442/lc442.js
+++ kelvinWu/cyclicSort/lc442/lc442.js
@@ -2,15 +2,54 @@
 
 // We are given an unsorted array containing ‘n’ numbers taken from the range 1 to ‘n’. The array has some numbers appearing twice, find all these duplicate numbers without using any extra space.
 
-const find_all_duplicates = function(nums) {
+// [1 2 3 4 5]
+// [3 4 4 5 5]
+
+// [3 4 3 4 5]
+
+// [3 4 4 5 5]
+// [4 4 3 5 5]
+// [5 4 3 4 5]
+// [5 4 3 4 5]
+
+//loop through array
+//check if position current number should be at is already occupied by this number
+////the position does not have the number, swap the nums at current index and its new position
+//else move on
+
+//loop through array again
+//check if current number is at the current position
+////if it is not, means it oculdnt be moved to its correct position because the position was already filled
+//////must be a duplicate then. add number to duplicate array
+
+// [3 4 3 4 5]
+
+const find_all_duplicates = function (nums) {
   duplicateNumbers = [];
   // TODO: Write your code here
+  let i = 0;
+  while (i < nums.length) {
+    let swapIndex = nums[i] - 1;
+
+    if (nums[swapIndex] !== nums[i]) {
+      let temp = nums[swapIndex];
+      nums[swapIndex] = nums[i];
+      nums[i] = temp;
+    } else {
+      i++;
+    }
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) {
+      duplicateNumbers.push(nums[i]);
+    }
+  }
   return duplicateNumbers;
 };
 
 console.log(find_all_duplicates([3, 4, 4, 5, 5]));
 console.log(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]));
-
 
 // Solution
 // -----
