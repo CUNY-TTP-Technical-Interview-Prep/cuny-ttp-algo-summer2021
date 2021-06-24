3d1aa7c6e8949ddb1a493086a1eb51a0b55d1ba2
rwnwong <rwnwong@gmail.com>
2021-06-24 11:49:57 -0400

    completed lc287.py

--- roseWong/assignments/cyclicSort/lc287/lc287.py
+++ roseWong/assignments/cyclicSort/lc287/lc287.py
@@ -1,15 +1,50 @@
 # Problem Statement #
 
-# We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
+# We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’.
+# The array has only one duplicate but it can be repeated multiple times. 
+# Find that duplicate number without using any extra space. 
+# You are, however, allowed to modify the input array.
 
-def find_duplicate(nums):
-  # TODO: Write your code here
+# using a cyclic sort method, loop through the array and sort until we find a duplicate, in which
+# case we return that duplicate value.
+# if no duplicate, return -1
+# if empty array, retunr -1
+
+# Pseudocode:
+# while i < len(array):
+# if array[i] == i+1:     index 0 value = 4  -> 4 to go to index 3, and value in index 3 to go to index i
+#  i++
+#  else 
+#   temp == array[i]
+#     if array[temp -1] == temp
+#       return temp
+#     else
+#       array[i] == array[temp -1]
+#       array[temp -1]=temp
+# return -1
+# 
+
+
+def find_duplicate(array):
+  i=0
+  while i < len(array):
+    if array[i] == i+1:
+      i = i+1
+    else:
+      temp = array[i]
+      if array[temp -1] == temp:
+        return temp
+      else:
+        array[i] = array[temp -1]
+        array[temp -1]=temp
   return -1
 
 def main():
-  print(find_duplicate([1, 4, 4, 3, 2]))
-  print(find_duplicate([2, 1, 3, 3, 5, 4]))
-  print(find_duplicate([2, 4, 1, 4, 4]))
+  print(find_duplicate([1, 4, 4, 3, 2]))        #4
+  print(find_duplicate([2, 1, 3, 3, 5, 4]))     #3
+  print(find_duplicate([2, 4, 1, 4, 4]))        #4
+  print(find_duplicate([]))                     #-1
+  print(find_duplicate([2, 2, 2, 2]))           #2
 
 
 main()
